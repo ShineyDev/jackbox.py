b6500ef138db035c803417d4c9328c8fc6c725c5
Riley Shaw <contact@shiney.dev>
2020-02-24 15:54:46 +0000

    add minimum documentation

---
+++ docs/conf.py
@@ -0,0 +1,55 @@
+"""
+/docs/conf.py
+"""
+
+import os
+import re
+import sys
+
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+# extensions
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib_trio",
+]
+
+autodoc_typehints = "none"
+
+intersphinx_mapping = {
+    "aiohttp": ("https://aiohttp.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3", None),
+    "websockets": ("https://websockets.readthedocs.io/en/stable/", None),
+}
+
+# main config
+
+highlight_language = "python3"
+html_experimental_html5_writer = True
+html_static_path = ["_static"]
+html_theme = "basic"
+master_doc = "index"
+pygments_style = "friendly"
+source_suffix = ".rst"
+
+# project information
+
+copyright = "2020, ShineyDev"
+project = "jackbox.py"
+
+with open("../jackbox/__init__.py", "r") as file_stream:
+    release = re.search(r"^__version__ = [\"]([^\"]*)[\"]", file_stream.read(), re.MULTILINE).group(1)
+
+# reST config
+
+rst_prolog = """
+.. |coro| replace:: This function is a |coroutine_link|_.
+.. |coroutine_link| replace:: *coroutine*
+.. _coroutine_link: https://docs.python.org/3/library/asyncio-task.html#coroutine
+"""
---
+++ docs/index.rst
@@ -0,0 +1,36 @@
+.. jackbox.py documentation master file
+
+
+jackbox.py
+==========
+
+An asynchronous Python framework for interacting with |jackbox_games|.
+
+
+Classes
+-------
+
+.. toctree::
+	:maxdepth: 2
+
+	
+
+.. toctree::
+	:maxdepth: 1
+
+	
+
+
+Other
+-----
+
+.. toctree::
+	:maxdepth: 1
+
+	
+
+
+Still can't find what you're looking for?
+
+* :ref:`genindex`
+* :ref:`search`
